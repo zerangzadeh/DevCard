e3b4ae56ecf5ed5ce940d4c57fb5baf07d5d8cb1
zerangzadeh <a.zerangzadeh@gmail.com>
2021-10-28 21:31:26 +0330

    create ProjectViewComponent and Desing Project View

---
+++ DevCard_MVC/Models/Project.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevCard_MVC.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public  string Description { get; set; }
+        public  string Client { get; set;}
+        public string ProjectImage { get; set; }
+
+
+        public Project(int id, string name, string description, string client, string projectImage)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Client = client;
+            ProjectImage = projectImage;
+        }
+    }
+}
