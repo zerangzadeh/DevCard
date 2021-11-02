f712355cafc1ed68c6b0a6f3e66637c5536a3967
zerangzadeh <a.zerangzadeh@gmail.com>
2021-11-02 20:00:01 +0330

    Server Side Form Validation Emplimented

--- DevCard_MVC/Controllers/HomeController.cs
+++ DevCard_MVC/Controllers/HomeController.cs
@@ -1,27 +1,16 @@
-﻿using DevCard_MVC.Models;
+﻿using System.Diagnostics;
+using DevCard_MVC.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 
 namespace DevCard_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        
-        public HomeController()
-        {
-           
-        }
-
         public IActionResult Index()
         {
             return View();
         }
+
         [HttpGet]
         public IActionResult Contact()
         {
@@ -30,15 +19,23 @@ namespace DevCard_MVC.Controllers
         }
 
         [HttpPost]
-        public JsonResult Contact(Contact form)
+        public IActionResult Contact(Contact form)
         {
-            //var name = form.Name;
-            //var email = form.Email;
-            //var subscribe = form.SubsrcibeType;
-            //var desc = form.Descreption;
-            Console.WriteLine(form.ToString());
-            return Json(Ok());
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Error = "اطلاعات وارد شده صحیح نیست";
+                return View(form);
+            }
+            else
+            {
+                ViewBag.Success = "اطلاعات وارد شده ثبت شد.";
+                return View();
+
+            }
+            
+
         }
+        
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
